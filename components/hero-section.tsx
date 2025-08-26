1025caf247cb3e84aae1e9e3b8fb08e6470b8ff8
Jesse Hernandez <jesseovr@gmail.com>
2025-08-26 10:11:44 -0500

    bottom adj

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -105,7 +105,7 @@ export default function NewLandingPage() {
 
             {/* Mobile Marquee - Below image */}
             <motion.div
-              className="absolute xs:bottom-12 left-0 right-0 bg-[#ca0013] 
+              className="absolute xs:bottom-13 left-0 right-0 bg-[#ca0013] 
                          py-4 xs:py-5 sm:py-6 overflow-hidden shadow-lg w-screen -ml-4 xs:-ml-4 sm:-ml-6"
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
